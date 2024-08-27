93b9e0759355d582ed3ea54b8cb8f2052a82c928
Prathihas <prathihasreddy88@gmail.com>
2024-08-27 10:37:49 -0700

    created a new folder section14 to implement apache kafka

---
+++ section14/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -0,0 +1,17 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    void createAccount(CustomerDto customerDto);
+
+    CustomerDto fetchAccount(String MobileNumber);
+
+    boolean updateAccount(CustomerDto customerDto);
+
+    boolean deleteAccount(String mobileNumber);
+
+    boolean updateCommunicationStatus(Long accountNumber);
+
+}
