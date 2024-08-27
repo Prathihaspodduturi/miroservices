b2d6097725fe794d8d17b341004fead22edf7cea
Prathihas <prathihasreddy88@gmail.com>
2024-08-26 18:20:50 -0700

    updated the function in AccountsFunction by calling the accountservice.updateCommunicationStatus to uodate the column in accounts table. It executes when a messsage is receieved from the rabbitMQ

--- section13/accounts/src/main/java/com/eazybytes/accounts/functions/AccountsFunctions.java
+++ section13/accounts/src/main/java/com/eazybytes/accounts/functions/AccountsFunctions.java
@@ -1,7 +1,9 @@
 package com.eazybytes.accounts.functions;
 
+import com.eazybytes.accounts.service.IAccountsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
@@ -11,10 +13,12 @@ public class AccountsFunctions {
 
     private static final Logger log = LoggerFactory.getLogger(AccountsFunctions.class);
 
-    public Consumer<Long> updateCommunication () {
+    @Bean
+    public Consumer<Long> updateCommunication (IAccountsService accountsService) {
 
         return accountNumber -> {
           log.info("Updating communication status for the account number: "+accountNumber.toString());
+          accountsService.updateCommunicationStatus(accountNumber);
 
 
         };
